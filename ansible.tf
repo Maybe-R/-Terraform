1b1c32cc37d8ed98f98c52352ee6c0d8ffe2271f
Maybe-R <rita.flid@yandex.ru>
2026-07-07 14:12:45 +0300

    Update ansible.tf

--- ansible.tf
+++ ansible.tf
@@ -1,9 +1,10 @@
 
-## resource "local_file" "ansible_inventory" {
-##  content = templatefile("${path.module}/templates/ansible_inventory.tftpl", {
-##    webservers = yandex_compute_instance.web
-##    prodservers = yandex_compute_instance.prod
-##    storage    = yandex_compute_instance.storage
-##  })
-##  filename = "${path.module}/inventory.ini"
-##}
+ resource "local_file" "ansible_inventory" {
+  content = templatefile("${path.module}/templates/ansible_inventory.tftpl", {
+    webservers = yandex_compute_instance.web
+    prodservers = yandex_compute_instance.prod
+    storage    = [yandex_compute_instance.storage]
+  })
+  filename = "${path.module}/ansible_inventory.cfg"
+ }
+
